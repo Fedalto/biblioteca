fb144d9c7cba154176ab6d82e3fdd8f7ae9a273d
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2012-07-14 17:49:49 -0300

    Menu showing up.
    Not checking if an option is valid yet.

--- src/Biblioteca.java
+++ src/Biblioteca.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class Biblioteca {
     public static void main(String[] args) {
         Biblioteca bibl = new Biblioteca();
         bibl.welcomeScreen();
+        bibl.mainMenu();
     }
 
     public void welcomeScreen() {
         System.out.println("Welcome to the Bangalore Public Library System");
+    }
+
+    public void showMenu() {
+        System.out.println("Menu:");
+        System.out.println("1) View all books");
+        System.out.println("2) Reserve a book");
+        System.out.println("3) Check library number");
+        System.out.println("Q) Quit");
+        System.out.print("Option: ");
+    }
+
+    // Menu loop
+    public void mainMenu() {
+        String choice;
+
+        do {
+            showMenu();
+            choice = getMenuChoice();
+
+            switch(choice) {
+                case "1":
+                    // View all books
+                    break;
+                case "2":
+                    // Reserve a book
+                    break;
+                case "3":
+                    // Check library number
+                    break;
+                case "q":
+                case "Q":
+                    // Quit
+                    break;
+            }
+
+        } while (!choice.equalsIgnoreCase("q"));
+    }
+
+    private String getMenuChoice() {
+        Scanner in = new Scanner(System.in);
+        return in.next();
     }
 }
--- test/BibliotecaTest.java
+++ test/BibliotecaTest.java
@@ -1,25 +1,42 @@
 import junit.framework.TestCase;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+
 public class BibliotecaTest extends TestCase {
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private ByteArrayInputStream in;
+    Biblioteca bibl;
 
     public void setUp() throws Exception {
-        System.setOut(new PrintStream(outContent));
+        System.setOut(new PrintStream(out));
+        bibl = new Biblioteca();
     }
 
     public void tearDown() throws Exception {
         System.setOut(null);
     }
 
+    // The customer should see a welcome when they start the application.
     public void testWelcomeScreen() throws Exception {
-        Biblioteca bibl = new Biblioteca();
         bibl.welcomeScreen();
-        assertEquals("Welcome to the Bangalore Public Library System\n", outContent.toString());
+        assertThat(out.toString(), containsString("Welcome to the Bangalore Public Library System"));
+    }
+
+    // A customer should have a list of menu options at the start of the application.
+    public void testShowMenu() throws Exception {
+        bibl.showMenu();
+        assertThat(out.toString(), containsString("Menu"));
+    }
+
+    // A customer should be able to select a menu option.
+    public void testSelectMenuOption() throws Exception {
+        in = new ByteArrayInputStream("q".getBytes());
+        System.setIn(in);
+        bibl.mainMenu();
     }
 }
